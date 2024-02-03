b6efa972c0c1006a26a1ca81554bf762a00de548
Sean <seankim912@yahoo.com>
2024-02-02 22:53:37 -0600

    setup for intro splash page

---
+++ react-vite/src/components/IntroSplash/IntroSplash.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import "./IntroSplash.css";
+
+function IntroSplash() {
+    return (
+        <>
+        </>
+    );
+}
+
+export default IntroSplash;
