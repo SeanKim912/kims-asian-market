d5d037ccdcb59929bbfaec373fe68bb321628576
Sean <seankim912@yahoo.com>
2024-02-07 17:03:19 -0600

    contacts page formatted

--- react-vite/src/components/ContactUs/ContactUs.jsx
+++ react-vite/src/components/ContactUs/ContactUs.jsx
@@ -3,14 +3,23 @@ import './ContactUs.css';
 
 function ContactUs() {
     return (
-        <>
+        <div className='contact-block'>
             <h2>We're here to help</h2>
-            <ul>
-                <li>Call Us:</li>
-                <li>Ask a Question:</li>
-                <li>Connect With Us:</li>
-            </ul>
-        </>
+            <div>
+                <ul className='contact-container'>
+                    <li>Call Us:</li>
+                    <li>(828) 254-7253</li>
+                </ul>
+                <ul className='contact-container'>
+                    <li>Ask a Question:</li>
+                    <li>kims.asiannmore@gmail.com</li>
+                </ul>
+                <ul className='contact-container'>
+                    <li>Connect With Us:</li>
+                    <li>(828) 254 7253</li>
+                </ul>
+            </div>
+        </div>
     );
 }
 
