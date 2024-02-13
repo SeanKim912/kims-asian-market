0e4b8246649de08a2e3fd4d4e6d49cc86ba2c4d1
Sean <seankim912@yahoo.com>
2024-02-12 23:09:22 -0600

    animations refactored to be in css file and not classname

--- react-vite/src/components/AboutUs/AboutUs.jsx
+++ react-vite/src/components/AboutUs/AboutUs.jsx
@@ -7,28 +7,20 @@ function AboutUs() {
             <div className="about-right"></div>
             <p className="about-left">
                 <span className="korean-text">
-                    <span className="animate__animated animate__fadeIn">
-                        안녕하세요
-                    </span>
+                    안녕하세요
                 </span>
                 <span className="english-text">
-                    <span className="animate__animated animate__fadeIn">
-                        (Hello)
-                    </span>
+                    (Hello)
                 </span>
                 and welcome to Kim's Asian Market, where the vibrant spirit of Korean-American heritage
                 meets the rich tapestry of Asian culture. As the proud owners we are thrilled to share our passion for
                 authentic flavors, diverse traditions, and the joy of exploring the culinary and cultural treasures of Asia
                 with Asheville!
                 <span className="korean-text">
-                    <span className="animate__animated animate__fadeIn">
-                        감사합니다
-                    </span>
+                    감사합니다
                 </span>
                 <span className="english-text">
-                    <span className="animate__animated animate__fadeIn">
                         (Thank you)
-                    </span>
                 </span> for being a part of our Kim's family!
             </p>
         </div>
