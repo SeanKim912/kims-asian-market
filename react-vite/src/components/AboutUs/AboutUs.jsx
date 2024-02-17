edbd488d5816daa9504a00210cb2f9bd49053691
Sean <seankim912@yahoo.com>
2024-02-16 22:37:33 -0600

    korean font uploaded

--- react-vite/src/components/AboutUs/AboutUs.jsx
+++ react-vite/src/components/AboutUs/AboutUs.jsx
@@ -8,20 +8,21 @@ function AboutUs() {
             <p className="about-left">
                 <span className="korean-text">
                     안녕하세요
-                </span>
+                </span>&nbsp;
                 <span className="english-text">
                     (Hello)
-                </span>
+                </span>&nbsp;
                 and welcome to Kim's Asian Market, where the vibrant spirit of Korean-American heritage
                 meets the rich tapestry of Asian culture. As the proud owners we are thrilled to share our passion for
                 authentic flavors, diverse traditions, and the joy of exploring the culinary and cultural treasures of Asia
                 with Asheville!
+                <span></span>&nbsp;
                 <span className="korean-text">
                     감사합니다
-                </span>
+                </span>&nbsp;
                 <span className="english-text">
                         (Thank you)
-                </span> for being a part of our Kim's family!
+                </span>&nbsp;for being a part of our Kim's family!
             </p>
         </div>
     );
