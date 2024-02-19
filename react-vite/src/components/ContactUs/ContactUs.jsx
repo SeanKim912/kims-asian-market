f2a9804290340045e911e9eee94de335bc580a98
Sean <seankim912@yahoo.com>
2024-02-18 22:28:04 -0600

    fully formatted, colored without images

--- react-vite/src/components/ContactUs/ContactUs.jsx
+++ react-vite/src/components/ContactUs/ContactUs.jsx
@@ -10,16 +10,16 @@ function ContactUs() {
             <h2 className="contact-header">We&apos;re here to help</h2>
             <div>
                 <ul className='contact-container'>
-                    <li>Call Us:</li>
+                    <li className="contact-section">Call Us:</li>
                     <li>(828) 254-7235</li>
                 </ul>
                 <ul className='contact-container'>
-                    <li>Ask a Question:</li>
+                    <li className="contact-section">Ask a Question:</li>
                     <li>kims.asiannmore@gmail.com</li>
                 </ul>
                 <ul className='contact-container'>
-                    <li>Connect With Us:</li>
-                    <li>
+                    <li className="contact-section">Connect With Us:</li>
+                    <li className="social-media-list">
                         <img src={instagram} className='social-icon' />
                         <img src={linkedin} className='social-icon' />
                         <img src={tiktok} className='social-icon' />
