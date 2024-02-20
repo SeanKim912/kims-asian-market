72461a04a0a76dbb2f6570606c82cb35c40b59fd
Sean <seankim912@yahoo.com>
2024-02-19 19:32:40 -0600

    products pictures added and formatted

--- react-vite/src/components/Products/Products.jsx
+++ react-vite/src/components/Products/Products.jsx
@@ -1,17 +1,23 @@
-import "./Products.css"
+import "./Products.css";
+import kimchi from '../../../public/images/kimchi.png';
+import kbeauty from '../../../public/images/kbeauty.png';
+import food from '../../../public/images/pan-asian-food.png';
 
 function Products() {
     return (
         <div className="products-container">
             <div className="products-category">
+                <img className="product-image" src={kimchi} />
                 <h2>Kimchi</h2>
                 <p>All kimchi made in-house, and sold to you fresh!</p>
             </div>
             <div className="products-category">
+                <img className="product-image" src={kbeauty} />
                 <h2>K-Beauty</h2>
                 <p>The latest in Korean skincare</p>
             </div>
             <div className="products-category">
+                <img className="product-image" src={food} />
                 <h2>Food from all of Asia</h2>
                 <p>Korea doesn&apos;t limit itself regionally in pursuit of a good meal, so why should we?
                     We also carry food and ingredients from China, Japan, and more!
