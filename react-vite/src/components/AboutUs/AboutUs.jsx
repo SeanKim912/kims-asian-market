c8665625768a869dc32176050b1fe9ef0556bbb5
Sean <seankim912@yahoo.com>
2024-03-03 23:42:58 -0600

    new photos uploaded, formatted

--- react-vite/src/components/AboutUs/AboutUs.jsx
+++ react-vite/src/components/AboutUs/AboutUs.jsx
@@ -1,32 +1,33 @@
 import "./AboutUs.css";
-import Map from "../Map";
 import "animate.css";
+import teaset from '../../images/teaset.png';
 
 function AboutUs() {
+    let backgroundStyles = {
+        backgroundImage: `url(${teaset})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat'
+    }
     return (
-        <div className="about-container">
-            <div className="about-left">
-                <Map />
+        <div className="about-container" style={backgroundStyles}>
+            <div className="gradient-overlay">
+                <div className="about-left" />
+                <p className="about-right">
+                    <span className="korean-text">안녕하세요</span>&nbsp;
+                    <span className="english-text">(Hello)</span>&nbsp;
+                    and welcome to&nbsp;
+                    <span style={{color: '#990000'}}>KIM&apos;S</span>&nbsp;
+                    <span style={{color: '#38761d'}}>ASIAN MARKET</span>
+                    , where the vibrant spirit of Korean-American heritage
+                    meets the rich tapestry of Asian culture. As the proud owners we are thrilled to share our passion for
+                    authentic flavors, diverse traditions, and the joy of exploring the culinary and cultural treasures of Asia
+                    with Asheville!&nbsp;
+                    <span className="korean-text">감사합니다</span>&nbsp;
+                    <span className="english-text">(Thank you)</span>&nbsp;
+                    for being a part of our Kim&apos;s family!
+                </p>
             </div>
-            <p className="about-right">
-                <span className="korean-text">
-                    안녕하세요
-                </span>&nbsp;
-                <span className="english-text">
-                    (Hello)
-                </span>&nbsp;
-                and welcome to Kim&apos;s Asian Market, where the vibrant spirit of Korean-American heritage
-                meets the rich tapestry of Asian culture. As the proud owners we are thrilled to share our passion for
-                authentic flavors, diverse traditions, and the joy of exploring the culinary and cultural treasures of Asia
-                with Asheville!
-                <span></span>&nbsp;
-                <span className="korean-text">
-                    감사합니다
-                </span>&nbsp;
-                <span className="english-text">
-                        (Thank you)
-                </span>&nbsp;for being a part of our Kim&apos;s family!
-            </p>
         </div>
     );
 }
--- react-vite/src/components/Products/Products.jsx
+++ react-vite/src/components/Products/Products.jsx
@@ -1,27 +1,43 @@
 import "./Products.css";
-import kimchi from '../../images/kimchi.png';
+import kimchi from '../../images/kimchi_closeup.png';
 import kbeauty from '../../images/kbeauty.png';
-import food from '../../images/pan-asian-food.png';
+import food from '../../images/asian_cuisine.png';
 
 function Products() {
     return (
         <div className="products-container">
             <div className="products-category">
-                <img className="product-image" src={kimchi} />
-                <h2>Kimchi</h2>
-                <p>All kimchi made in-house since 1981, and sold to you fresh!</p>
+                <div
+                    className="product-image"
+                    style={{
+                        backgroundImage: `url(${kimchi})`,
+                        width: "33vw"
+                    }}>
+                    <h2>Kimchi</h2>
+                    <p>Authentic homemade kimchi sold to you fresh!</p>
+                </div>
             </div>
             <div className="products-category">
-                <img className="product-image" src={kbeauty} />
-                <h2>K-Beauty</h2>
-                <p>The latest in Korean skincare</p>
+                <div
+                    className="product-image"
+                    style={{
+                        backgroundImage: `url(${kbeauty})`,
+                        width: "34vw"
+                    }}>
+                    <h2>K-Beauty</h2>
+                    <p>The latest in Korean skincare and cosmetics!</p>
+                </div>
             </div>
             <div className="products-category">
-                <img className="product-image" src={food} />
-                <h2>Food from all of Asia</h2>
-                <p>Korea doesn&apos;t limit itself regionally in pursuit of a good meal, so why should we?
-                    We also carry food and ingredients from China, Japan, and more!
-                </p>
+                <div
+                    className="product-image"
+                    style={{
+                        backgroundImage: `url(${food})`,
+                        width: "33vw"
+                    }}>
+                    <h2>Food from all of Asia</h2>
+                    <p>We carry food and ingredients from Korea, Japan, and more!</p>
+                </div>
             </div>
         </div>
     );
