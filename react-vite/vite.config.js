5b5cc0f4a70e96e9751227211406911901a9c3e4
Sean <seankim912@yahoo.com>
2024-03-06 21:39:20 -0600

    vite.config.js modified for google maps api

--- react-vite/vite.config.js
+++ react-vite/vite.config.js
@@ -4,6 +4,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig((mode) => ({
+  build: {
+    rollupOptions: {
+      external: ["@react-google-maps/api"],
+    }
+  },
   plugins: [
     react(),
     eslintPlugin({
